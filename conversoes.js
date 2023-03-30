373bd1bed2a605090c267bb2f26f023a2808499f
Anderson Lemes dos Santos <alemes@ciandt.com>
2023-03-30 11:42:50 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado
+//booleanos
+
+//conversao implicita
+
+const numero = 456;
+const numeroString = Number("456") ;
+
+console.log(numero === numeroString);
+console.log(numero == numeroString);
+
+
+//Number()
+//String()
+console.log(numero + numeroString);
+//console.log (numero + Number(numeroString))
+//conversao explicita
+
---
+++ truthy-falsy.js
@@ -0,0 +1,20 @@
+// boolean 
+//const usuarioLogado = true 
+//const contaPaga = false;
+
+// truthy ou falsy
+
+// 0 = false
+// 1 = true
+
+//console.log(0 == false)
+
+let minhaVar;
+let varNull = null;
+let  numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+console.log(typeof numero)
+console.log(typeof texto)
